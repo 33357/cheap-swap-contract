46cfb5b96c7f0c6245fbd64cf07c0d2ba4ad2597
33357 <1043898730@qq.com>
2022-06-27 19:31:55 -0700

    0.0.1

---
+++ test/path.test.ts
@@ -0,0 +1,27 @@
+const uniswap = {
+    encodePath(path: string[], fees: Array<number>): string {
+      if (path.length != fees.length + 1) {
+        throw new Error('path/fee lengths do not match');
+      }
+      let encoded = '0x';
+      for (let i = 0; i < fees.length; i++) {
+        encoded += path[i].slice(2);
+        encoded += fees[i].toString(16).padStart(2 * 3, '0');
+      }
+      encoded += path[path.length - 1].slice(2);
+      return encoded.toLowerCase();
+    },
+  };
+
+function main(){
+    const PATH = ["0xdAC17F958D2ee523a2206206994597C13D831ec7", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"];
+    const FEE = [500];
+    const bytes = uniswap.encodePath(PATH, FEE);
+    console.log(bytes);
+}
+
+main();
+
+
+
+  
