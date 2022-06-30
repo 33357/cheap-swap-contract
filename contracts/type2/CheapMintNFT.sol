24268c33523831385edcbaa82aa37706d0e16b53
33357 <1043898730@qq.com>
2022-06-30 07:26:35 -0700

    0.0.1

--- contracts/type2/CheapMintNFT.sol
+++ contracts/type2/CheapMintNFT.sol
@@ -13,7 +13,7 @@ contract CheapMintNFT {
 
     /* ================ TRANSACTION FUNCTIONS ================ */
 
-    function mintNFT(bytes calldata mintNFTData) public {
+    function mintNFT(bytes calldata mintNFTData) external {
         uint8 perContractMint = mintNFTData.toUint8(0);
         uint8 totalContract = mintNFTData.toUint8(1);
         address target = mintNFTData.toAddress(2);
@@ -25,6 +25,28 @@ contract CheapMintNFT {
                 cheapSwapAddress.owner(),
                 mintNFTData.slice(22, mintNFTData.length - 22)
             );
+        }
+    }
+
+    function getAddressNFT(
+        address target,
+        address[] calldata contractList,
+        uint256[] calldata tokenId
+    ) external {
+        IERC721 nft = IERC721(target);
+        for (uint256 i = 0; i < contractList.length; i++) {
+            nft.transferFrom(contractList[i], msg.sender, tokenId[i]);
+        }
+    }
+
+    function getNFT(
+        address target,
+        address contractAddress,
+        uint256[] calldata tokenId
+    ) external {
+        IERC721 nft = IERC721(target);
+        for (uint256 i = 0; i < tokenId.length; i++) {
+            nft.transferFrom(contractAddress, msg.sender, tokenId[i]);
         }
     }
 }
@@ -39,10 +61,12 @@ contract MintNFT is IERC721Receiver {
         bytes memory mintData
     ) {
         owner = _owner;
+        IERC721 nft = IERC721(target);
         for (uint8 i = 0; i < perContractMint; i++) {
             (bool success, ) = target.call(mintData);
             require(success, "cheapMintNFT: mint NFT error");
         }
+        nft.setApprovalForAll(owner, true);
         selfdestruct(payable(msg.sender));
     }
 
@@ -54,7 +78,10 @@ contract MintNFT is IERC721Receiver {
         uint256 tokenId,
         bytes calldata data
     ) external override returns (bytes4) {
-        IERC721(msg.sender).transferFrom(address(this), owner, tokenId);
-        return IERC721Receiver.onERC721Received.selector;
+        return this.onERC721Received.selector;
     }
 }
+
+// 0x0101c078d7461c712308DFF400CD0e47E6a6955bA9fba0712d68000000000000000000000000000000000000000000000000000000000000000a
+
+// 0x45AD1518625a9e75627c07C22198E7C6d10a457c52c253c00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000003a0101c078d7461c712308dff400cd0e47e6a6955ba9fba0712d68000000000000000000000000000000000000000000000000000000000000000a000000000000
