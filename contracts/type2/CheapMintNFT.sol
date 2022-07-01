41f1ee9c819017140230a11803a348bc30e3a8f7
33357 <1043898730@qq.com>
2022-07-01 02:04:02 -0700

    0.0.1

--- contracts/type2/CheapMintNFT.sol
+++ contracts/type2/CheapMintNFT.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.12;
 import "../lib/CheapMintNFTBytesLib.sol";
 import "./interfaces/ICheapSwapAddress.sol";
 import "./interfaces/ICheapMintNFT.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract CheapMintNFT is ICheapMintNFT {
@@ -26,10 +25,9 @@ contract CheapMintNFT is ICheapMintNFT {
             owner = cheapSwapAddress.owner();
         }
         for (uint8 i = 0; i < total; i++) {
-            MintNFT mintNFT = new MintNFT();
-            mintNFT.mint(perMint, target, owner, mintNFTData.slice(22, mintNFTData.length - 22));
-            mintNFTOwner[address(mintNFT)] = owner;
-            emit MintNFTCreated(owner, address(mintNFT));
+            address mintNFT = address(new MintNFT(perMint, target, mintNFTData.slice(22, mintNFTData.length - 22)));
+            mintNFTOwner[mintNFT] = owner;
+            emit MintNFTCreated(owner, mintNFT);
         }
     }
 
@@ -58,36 +56,17 @@ contract CheapMintNFT is ICheapMintNFT {
     }
 }
 
-contract MintNFT is IERC721Receiver {
-    address owner;
-
-    constructor() {}
-
-    function mint(
+contract MintNFT {
+    constructor(
         uint8 perMint,
         address target,
-        address _owner,
         bytes memory mintData
-    ) external {
-        owner = _owner;
-        IERC721 nft = IERC721(target);
+    ) {
         for (uint8 i = 0; i < perMint; ++i) {
             (bool success, ) = target.call(mintData);
             require(success, "cheapMintNFT: mint NFT error");
         }
-        nft.setApprovalForAll(msg.sender, true);
+        IERC721(target).setApprovalForAll(msg.sender, true);
         selfdestruct(payable(msg.sender));
-    }
-
-    /* ================ TRANSACTION FUNCTIONS ================ */
-
-    function onERC721Received(
-        address operator,
-        address from,
-        uint256 tokenId,
-        bytes calldata data
-    ) external override returns (bytes4) {
-        IERC721(msg.sender).safeTransferFrom(address(this), owner, tokenId);
-        return this.onERC721Received.selector;
     }
 }
