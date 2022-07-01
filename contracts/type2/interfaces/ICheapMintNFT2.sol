cd651f25f3537a0d05e1e468b8a3e5b10ed6d829
33357 <1043898730@qq.com>
2022-07-01 05:40:15 -0700

    0.0.1

--- contracts/type2/interfaces/ICheapMintNFT2.sol
+++ contracts/type2/interfaces/ICheapMintNFT2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-interface ICheapMintNFT {
+interface ICheapMintNFT2 {
     /* ================ TRANSACTION FUNCTIONS ================ */
 
     function mint() external;
