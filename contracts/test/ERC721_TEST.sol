24f9bd9757aab21047956ccc8e1900caa313fef1
33357 <1043898730@qq.com>
2022-07-02 04:44:43 -0700

    0.0.1

--- contracts/test/ERC721_TEST.sol
+++ contracts/test/ERC721_TEST.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ERC721_TEST is ERC721 {
     uint256 public nextTokenId;
+    uint256 public price = 100;
 
     constructor() ERC721("ERC721_TEST", "ERC721") {}
 
@@ -16,6 +17,14 @@ contract ERC721_TEST is ERC721 {
     }
 
     function safeMint(uint256 quality) external {
+        for (uint256 i = 0; i < quality; i++) {
+            _safeMint(msg.sender, nextTokenId);
+            nextTokenId++;
+        }
+    }
+
+    function priceMint(uint256 quality) external payable {
+        require(msg.value >= price * quality, "ERC721_TEST: error value");
         for (uint256 i = 0; i < quality; i++) {
             _safeMint(msg.sender, nextTokenId);
             nextTokenId++;
