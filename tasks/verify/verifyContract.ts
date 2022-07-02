effa1be816152a04c3a2bca1da3b10faf2fc9257
33357 <1043898730@qq.com>
2022-07-02 04:00:28 -0700

    0.0.1

--- tasks/verify/verifyContract.ts
+++ tasks/verify/verifyContract.ts
@@ -1,24 +1,24 @@
 import '@nomiclabs/hardhat-ethers';
-import {task} from 'hardhat/config';
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import { task } from 'hardhat/config';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import * as utils from '../utils';
 
 task(`contract:verify`, `verify contract`)
   .addOptionalParam('contract', 'The contract name')
   .addOptionalParam('args', 'The contract args')
+  .addParam('address', 'The contract address')
   .setAction(async (args, hre: HardhatRuntimeEnvironment) => {
     const contract = args['contract'];
-    const _args = JSON.parse(args['args']);
-
+    const contractArgs = JSON.parse(args['args']);
     const deployment = await utils.getDeployment(
       Number(await hre.getChainId())
     );
-
+    const address = args['address'] ? args['address'] : deployment[contract].implAddress;
     utils.log.info(
-      `verify ${contract},implAddress: ${deployment[contract].implAddress}`
+      `verify ${contract}, address: ${address}`
     );
     await hre.run('verify:verify', {
-      address: deployment[contract].implAddress,
-      constructorArguments: _args,
+      address: address,
+      constructorArguments: contractArgs,
     });
   });
