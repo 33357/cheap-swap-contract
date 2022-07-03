9f8bcd090b8bfbdf3a92325f324bb478c917625b
33357 <1043898730@qq.com>
2022-07-03 08:38:50 -0700

    0.0.1

--- test/cheapMintNFT.test.ts
+++ test/cheapMintNFT.test.ts
@@ -5,83 +5,55 @@ async function main() {
   console.log(
     `--------------------------------------safeMint--------------------------------------------`
   );
-  const ethAmount = ethers.utils.parseEther('0.0042').toString();
-  const deployment = await getDeployment(1);
-  const mintNFTAddress = deployment['CheapMintNFT'].implAddress;
-  const mintNFTSelector = '0x1249c58b';
-  const mintAmount = 1;
-  const nftAddress = '0xE94441705cEA3876908E9eDD9BcC67D12d77eF28';
-  const nftSelector = '0xa0712d68';
-  const value = ethers.utils.parseEther('0');
-  const perValue = ethers.utils.parseEther('0');
-  const deadline = Math.ceil(new Date().getTime() / 1000) + 60 * 60;
-  let cheapMintNFTCode =
-    mintNFTSelector +
-    numToHex(mintAmount, 2) +
-    delete0x(nftAddress) +
-    (value.eq(0) ? '' : bigToHex(perValue, 20)) +
-    delete0x(nftSelector);
-  let cheapSwapAddressCode =
-    '0x' +
-    numToHex(deadline, 10) +
-    mintNFTAddress +
-    delete0x(bigToHex(value, 20)) +
-    delete0x(cheapMintNFTCode);
-  console.log({ cheapMintNFTCode, cheapSwapAddressCode, ethAmount });
+  const deploymentMain = await getDeployment(1);
+  const main = {
+    msgValue: ethers.utils.parseEther('0.0042').toString(),
+    maxRunTime: 1,
+    deadline: Math.ceil(new Date().getTime() / 1000) + 60 * 60,
+    cheapMintNFTAddress: deploymentMain['CheapMintNFT'].implAddress,
+    value: ethers.utils.parseEther('0'),
+    cheapMintNFTSelector: '0x1249c58b',
+    nftAddress: '0xBD14cFf6ed9A1a44d2B7028D2dA04aa009975A3c',
+    mintValue: ethers.utils.parseEther('0'),
+    nftSelector: '0x31c864e8',
+    mintAmount: 2
+  }
+  logData(main)
 
   console.log(
     `--------------------------------------priceMint--------------------------------------------`
   );
-  const ethAmount2 = ethers.utils.parseEther('0.0043').toString();
-  const deployment2 = await getDeployment(1);
-  const mintNFTAddress2 = deployment2['CheapMintNFT'].implAddress;
-  const mintNFTSelector2 = '0x1249c58b';
-  const mintAmount2 = 1;
-  const nftAddress2 = '0xE94441705cEA3876908E9eDD9BcC67D12d77eF28';
-  const nftSelector2 = '0xa0712d68';
-  const value2 = ethers.utils.parseEther('0.01');
-  const perValue2 = ethers.utils.parseEther('0.01');
-  const deadline2 = Math.ceil(new Date().getTime() / 1000) + 60 * 60;
-  const cheapMintNFTCode2 =
-    mintNFTSelector2 +
-    numToHex(mintAmount2, 2) +
-    delete0x(nftAddress2) +
-    (value2.eq(0) ? '' : bigToHex(perValue2, 20)) +
-    delete0x(nftSelector2);
-  const cheapSwapAddressCode2 =
-    '0x' +
-    numToHex(deadline2, 10) +
-    mintNFTAddress2 +
-    delete0x(bigToHex(value2, 20)) +
-    delete0x(cheapMintNFTCode2);
-  console.log({ cheapMintNFTCode2, cheapSwapAddressCode2, ethAmount2 });
+  const main2 = {
+    msgValue: ethers.utils.parseEther('0.0043').toString(),
+    maxRunTime: 1,
+    deadline: Math.ceil(new Date().getTime() / 1000) + 60 * 60,
+    cheapMintNFTAddress: deploymentMain['CheapMintNFT'].implAddress,
+    value: ethers.utils.parseEther('0.01'),
+    cheapMintNFTSelector: '0x1249c58b',
+    nftAddress: '0xBD14cFf6ed9A1a44d2B7028D2dA04aa009975A3c',
+    mintValue: ethers.utils.parseEther('0.001'),
+    nftSelector: '0x0152b8c8',
+    mintAmount: 2
+  }
+  logData(main2)
 
   console.log(
     `--------------------------------------Test safeMint--------------------------------------------`
   );
-  const ethAmountTest = ethers.utils.parseEther('0.0042').toString();
   const deploymentTest = await getDeployment(97);
-  const mintNFTAddressTest = deploymentTest['CheapMintNFT'].implAddress;
-  const mintNFTSelectorTest = '0x1249c58b';
-  const mintAmountTest = 2;
-  const nftAddressTest = deploymentTest['ERC721_TEST'].implAddress;
-  const nftSelectorTest = '0x31c864e8';
-  const valueTest = BigNumber.from(0);
-  const perValueTest = BigNumber.from(0);
-  const deadlineTest = Math.ceil(new Date().getTime() / 1000) + 60 * 60;
-  const cheapMintNFTCodeTest =
-    mintNFTSelectorTest +
-    numToHex(mintAmountTest, 2) +
-    delete0x(nftAddressTest) +
-    (valueTest.eq(0) ? '' : bigToHex(perValueTest, 20)) +
-    delete0x(nftSelectorTest);
-  const cheapSwapAddressCodeTest =
-    '0x' +
-    numToHex(deadlineTest, 10) +
-    delete0x(mintNFTAddressTest) +
-    bigToHex(valueTest, 20) +
-    delete0x(cheapMintNFTCodeTest);
-  console.log({ cheapMintNFTCodeTest, cheapSwapAddressCodeTest, ethAmountTest });
+  const test = {
+    msgValue: ethers.utils.parseEther('0.0042').toString(),
+    maxRunTime: 1,
+    deadline: Math.ceil(new Date().getTime() / 1000) + 60 * 60,
+    cheapMintNFTAddress: deploymentTest['CheapMintNFT'].implAddress,
+    value: BigNumber.from(0),
+    cheapMintNFTSelector: '0x1249c58b',
+    nftAddress: deploymentTest['ERC721_TEST'].implAddress,
+    mintValue: BigNumber.from(0),
+    nftSelector: '0x31c864e8',
+    mintAmount: 2
+  }
+  logData(test)
 
   console.log(
     `--------------------------------------Test priceMint--------------------------------------------`
@@ -98,22 +70,7 @@ async function main() {
     nftSelector: '0x0152b8c8',
     mintAmount: 2
   }
-  let data =
-    test2.cheapMintNFTSelector +
-    delete0x(test2.nftAddress) +
-    (test2.value.eq(0) ? '' : bigToHex(test2.mintValue, 20)) +
-    delete0x(test2.nftSelector) +
-    numToHex(test2.mintAmount, 2);
-
-  console.log({
-    cheapMintNFTCode,
-    msgValue: test2.msgValue,
-    maxRunTime: test2.maxRunTime,
-    deadline: test2.deadline,
-    target: test2.cheapMintNFTAddress,
-    value: test2.value,
-    data: data,
-  });
+  logData(test2)
 }
 
 main();
@@ -136,4 +93,19 @@ function bigToHex(big: BigNumber, fixed: number) {
 
 function delete0x(str: string) {
   return str.replace('0x', '');
+}
+
+function logData(obj: any) {
+  console.log({
+    msgValue: obj.msgValue,
+    maxRunTime: obj.maxRunTime,
+    deadline: obj.deadline,
+    target: obj.cheapMintNFTAddress,
+    value: obj.value.toString(),
+    data: obj.cheapMintNFTSelector +
+      delete0x(obj.nftAddress) +
+      (obj.value.eq(0) ? '' : bigToHex(obj.mintValue, 20)) +
+      delete0x(obj.nftSelector) +
+      numToHex(obj.mintAmount, 2)
+  })
 }
