d28b80dd92492c38b539ab0feb8951d3b12f5eb0
33357 <1043898730@qq.com>
2022-07-03 08:14:46 -0700

    0.0.1

--- contracts/CheapMintNFT.sol
+++ contracts/CheapMintNFT.sol
@@ -81,7 +81,7 @@ contract CheapMintNFT is ICheapMintNFT {
         uint256 tokenId,
         bytes calldata
     ) external pure override returns (bytes4) {
-        revert(string(abi.encode(uint32(tokenId))));
+        revert(string(abi.encodePacked(uint32(tokenId))));
     }
 }
 
